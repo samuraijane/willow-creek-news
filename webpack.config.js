0b0c4efd448b14259490c2f42407ff8f8340ce88
Matthew Day <matt@matthewday.net>
2023-05-19 12:39:08 -0400

    include index.html in build that also uses the build for JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "build")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html")
+    })
+  ]
 };
