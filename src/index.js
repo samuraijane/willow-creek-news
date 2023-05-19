8db53fbb1e45a5c3072cf98e858fd02307571488
Matthew Day <matt@matthewday.net>
2023-05-19 13:05:52 -0400

    add some es6 JS to confirm babel works as expected

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const sayHelloManyTimes = (times) =>
+  new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+
 const helloDiv = document.createElement("div");
-helloDiv.innerHTML = "Hello from Javascript!";
+helloDiv.innerHTML = sayHelloManyTimes(10).join("<br/>");
 document.body.append(helloDiv);
