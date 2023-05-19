5e61fb8eab2f27d3a1dcb8c33a8dca29f35b5333
Matthew Day <matt@matthewday.net>
2023-05-19 12:51:26 -0400

    reorder keys in webpack config to be alpha

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build")
+    },
+    port: 3000
+  },
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -11,11 +17,5 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html")
     })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build")
-    },
-    port: 3000
-  }
+  ]
 };
