77265e953db0e9be5e1a39a5894327c6b3ca4df1
Matthew Day <matt@matthewday.net>
2023-05-19 14:10:55 -0400

    integrate the first react component with the application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const sayHelloManyTimes = (times) =>
-  new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app";
 
-const helloDiv = document.createElement("div");
-helloDiv.innerHTML = sayHelloManyTimes(10).join("<br/>");
-document.body.append(helloDiv);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
