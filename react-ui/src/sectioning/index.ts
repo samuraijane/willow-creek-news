521fde4f9331cbff9fc9111ef2381ca507662ef4
Matthew Day <matt@matthewday.net>
2023-09-01 14:54:42 -0400

    next sectioning components within their own directories

--- react-ui/src/sectioning/index.ts
+++ react-ui/src/sectioning/index.ts
@@ -1,5 +1,5 @@
-import Footer from "./footer";
-import Header from "./header";
-import Main from "./main";
+import Footer from "./footer/footer";
+import Header from "./header/header";
+import Main from "./main/main";
 
 export { Footer, Header, Main };
