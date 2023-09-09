d618e4ce3babea55784d2c91978f33f7e3265fb1
Matthew Day <matt@matthewday.net>
2023-09-09 18:19:15 -0400

    update URL for images

--- react-ui/src/components/news/news.tsx
+++ react-ui/src/components/news/news.tsx
@@ -7,7 +7,7 @@ const News = (): JSX.Element => {
       <ul className='news__list'>
         <li>
           <div className='news__title'>Book Drive</div>
-          <p>In partnership with JustServe, the Marietta Stake is sponsoring a book drive to benefit multiple organizations in our area. The drive has been extended one week until September 11, 2023. See the <a href="">flyer</a> for more information.</p>
+          <p>In partnership with JustServe, the Marietta Stake is sponsoring a book drive to benefit multiple organizations in our area. The drive has been extended one week until September 11, 2023. See the <a href="https://res.cloudinary.com/samuraijane/image/upload/v1626933506/wcnews/images/20230820-book-drive.jpg">flyer</a> for more information.</p>
         </li>
       </ul>
     </div>
