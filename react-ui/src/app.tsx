8b9a544953654f467e8c3ce5c50470beb0f4a50d
Matthew Day <matt@matthewday.net>
2023-09-10 17:50:32 -0400

    have the client call the API in order to track views

--- react-ui/src/app.tsx
+++ react-ui/src/app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Footer, Header, Main } from "./sectioning";
 import { useViewport } from './hooks/useViewport';
 
@@ -5,6 +6,19 @@ const App = (): JSX.Element => {
   const { screenSize, width } = useViewport();
   const isMobileView = width && width < 992;
   (window as any).WCN = {isMobileView, screenSize}; // TODO determine if we really need this
+
+  interface Response {
+    message: string;
+  }
+
+  useEffect(() => {
+    const doCheckIn = async () => {
+      const data = await fetch('/check-in');
+      const response: Response = await data.json();
+      console.log(response.message);
+    };
+    doCheckIn();
+  }, []);
 
   return (
     <>
